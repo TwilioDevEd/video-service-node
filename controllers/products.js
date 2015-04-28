3b76cd979823f805818edd349a53f6bf594e4a18
Kevin Whinnery <kevin.whinnery@gmail.com>
2015-04-28 17:18:09 -0500

    add detail page

--- controllers/products.js
+++ controllers/products.js
@@ -5,10 +5,23 @@ var swapi = 'http://swapi.co/api/starships';
 exports.list = function(request, response) {
     get(swapi, function(err, nodeClientResponse, body) {
         if (err) {
-            return response.status(500, err);
+            return response.status(500).send(err);
         }
 
         var data = JSON.parse(body);
         response.render('index', data);
+    });
+};
+
+// Show details for a specific product
+exports.detail = function(request, response) {
+    var url = request.query.url;
+    get(url, function(err, nodeClientResponse, body) {
+        if (err) {
+            return response.status(500).send(err);
+        }
+
+        var data = JSON.parse(body);
+        response.render('detail', data);
     });
 };
--- controllers/router.js
+++ controllers/router.js
@@ -1,7 +1,18 @@
 var products = require('./products');
+var s = require('underscore.string');
 
 // Map routes to controller functions
 module.exports = function(app) {
+    // Include string format helpers
+    app.locals.num = function(num, digits) {
+        var sigDigits = Number(digits);
+        if (isNaN(sigDigits)) sigDigits = 2;
+        return s.numberFormat(Number(num), sigDigits);
+    };
+
     // Render home page (product list)
     app.get('/', products.list);
+
+    // Product detail page
+    app.get('/products', products.detail);
 };
